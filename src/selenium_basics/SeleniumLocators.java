7e8d4d8849bef027e07215d22fc097d4976fde36
AmithaCh <amithachidurala91@gmail.com>
2018-10-18 15:32:27 -0400

    added one more class to the project

---
+++ src/selenium_basics/SeleniumLocators.java
@@ -0,0 +1,28 @@
+package selenium_basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumLocators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","E:\\Selenium prerequistes\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.id("q")).click();
+		driver.findElement(By.name("q")).click();
+		driver.findElement(By.className("q"));
+		driver.findElement(By.tagName("a"));
+		driver.findElement(By.linkText("Gmail"));
+		driver.findElement(By.partialLinkText("Gma"));
+		driver.findElement(By.xpath("//input[@id='q']"));
+		driver.findElement(By.cssSelector("input[id='q']"));
+		driver.close();
+		
+		
+		
+	}
+
+}
